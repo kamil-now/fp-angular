1c713e7ef17ef0582d597ce33c026434df0be1a8
Kamil Nowak <k.nowak3003@gmail.com>
2022-04-06 20:21:54 +0200

    Add navigation

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class NavigationComponent {
+  navigation = [
+    {
+      link: '/' ,
+      title: 'Home'
+    },
+    {
+      link:'/contact',
+      title: 'Contact'
+    }
+  ]
 }
