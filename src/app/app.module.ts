e2fdf9993b9b44b498694e22c67225c8fbfcf5cd
Kamil Nowak <k.nowak3003@gmail.com>
2022-04-06 20:30:05 +0200

    Move components to pages folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { ContactComponent } from './contact/contact.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContactComponent } from './pages/contact/contact.component'
+import { HomeComponent } from './pages/home/home.component'
+import { NavigationComponent } from './components/navigation/navigation.component'
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component'
 
 @NgModule({
   declarations: [
