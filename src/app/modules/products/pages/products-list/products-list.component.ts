fa877e98ce6715a9b41369ac02c40be6d1c6eba7
Kamil Nowak <k.nowak3003@gmail.com>
2022-04-16 18:48:01 +0200

    Add sample data to products list

--- src/app/modules/products/pages/products-list/products-list.component.ts
+++ src/app/modules/products/pages/products-list/products-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { Product } from 'src/app/models/product'
 
 @Component({
   selector: 'app-products-list',
@@ -6,4 +7,21 @@ import { Component } from '@angular/core'
   styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent {
+  products: Product[] = [
+    {
+      name:  'Product #1',
+      description: 'Product #1 description',
+      price: 1.23
+    },
+    {
+      name:  'Product #2',
+      description: 'Product #2 description',
+      price: 0
+    },
+    {
+      name:  'Product #3',
+      description: 'Product #3 description',
+      price: 3.23
+    }
+  ]
 }
