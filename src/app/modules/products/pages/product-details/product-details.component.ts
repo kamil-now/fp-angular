43f2e68e1ade1e4e3790aa3da9eef23433c5412f
Kamil Nowak <k.nowak3003@gmail.com>
2022-04-17 09:50:37 +0200

    Add content to product details page

--- src/app/modules/products/pages/product-details/product-details.component.ts
+++ src/app/modules/products/pages/product-details/product-details.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { mergeMap, Observable, pluck } from 'rxjs'
+import { Product } from 'src/app/models/product'
+import { ProductService } from 'src/app/modules/core/services/product.service'
 
 @Component({
   selector: 'app-product-details',
@@ -6,4 +10,17 @@ import { Component } from '@angular/core'
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent {
+
+  get product$(): Observable<Product | undefined> {
+    return this._activatedRoute.params.pipe(
+      pluck('id'),
+      mergeMap(id => this._productService.getProductById(id))
+    )
+  }
+
+  constructor(
+    private readonly _activatedRoute: ActivatedRoute,
+    private readonly _productService: ProductService
+  ) {
+  }
 }
