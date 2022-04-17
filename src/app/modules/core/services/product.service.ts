1328f956d4caab598cb5e1a95c725554a735a8fa
Kamil Nowak <k.nowak3003@gmail.com>
2022-04-17 09:48:42 +0200

    Return sample data from ProductService

--- src/app/modules/core/services/product.service.ts
+++ src/app/modules/core/services/product.service.ts
@@ -1,16 +1,37 @@
 import { Injectable } from '@angular/core'
-import { Observable } from 'rxjs'
+import { Observable, of } from 'rxjs'
 import { Product } from 'src/app/models/product'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-  getAllproducts(): Observable<Product[]>{
-    throw new Error('not implemented')
-  } 
-  
-  getProductById(): Observable<Product[]>{
-    throw new Error('not implemented')
-  } 
+  private _products: Product[] = [
+    {
+      id: '1',
+      name: 'Product #1',
+      description: 'Product #1 description',
+      price: 1.23
+    },
+    {
+      id: '2',
+      name: 'Product #2',
+      description: 'Product #2 description',
+      price: 0
+    },
+    {
+      id: '3',
+      name: 'Product #3',
+      description: 'Product #3 description',
+      price: 3.23
+    }
+  ]
+
+  getAllProducts(): Observable<Product[]> {
+    return of(this._products)
+  }
+
+  getProductById(id: string): Observable<Product | undefined> {
+    return of(id ? this._products.find(x => x.id === id) : undefined)
+  }
 }
--- src/app/modules/products/pages/products-list/products-list.component.ts
+++ src/app/modules/products/pages/products-list/products-list.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core'
-import { Product } from 'src/app/models/product'
+import { ProductService } from 'src/app/modules/core/services/product.service'
 
 @Component({
   selector: 'app-products-list',
@@ -7,21 +7,8 @@ import { Product } from 'src/app/models/product'
   styleUrls: ['./products-list.component.scss']
 })
 export class ProductsListComponent {
-  products: Product[] = [
-    {
-      name:  'Product #1',
-      description: 'Product #1 description',
-      price: 1.23
-    },
-    {
-      name:  'Product #2',
-      description: 'Product #2 description',
-      price: 0
-    },
-    {
-      name:  'Product #3',
-      description: 'Product #3 description',
-      price: 3.23
-    }
-  ]
+  constructor(
+    public readonly productService: ProductService
+    ) {
+  }
 }
